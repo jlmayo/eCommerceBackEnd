4cf58f377f654337755ac39e791a37e7f4a6a6dc
jlmayo <jlmayo22@gmail.com>
2022-11-02 13:53:57 -0700

    Created relationships in models. 1:1, 1:+1

--- models/index.js
+++ models/index.js
@@ -3,14 +3,25 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const sequelize = require('sequelize');
 
-// Products belongsTo Category
+// Products belongsTo Category**
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Categories have many Products
+// Categories have many Products**
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belongToMany Tags (through ProductTag)**
+Product.belongsToMany(Tag, { through: ProductTag });
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belongToMany Products (through ProductTag)**
+Tag.belongsToMany(Product, { through: ProductTag });
+
 
 module.exports = {
   Product,
