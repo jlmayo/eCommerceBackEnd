6edb38a04d1fff1fc55bbabc51714bc09392f4ab
jlmayo <jlmayo22@gmail.com>
2022-11-02 21:39:56 -0700

    Updated product and tag routes.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -21,6 +21,10 @@ router.get('/', async (req, res) => {
 // get one product
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
+//*****const travellerData = await Traveller.findByPk(req.params.id, {
+    // JOIN with locations, using the Trip through table
+   // include: [{ model: Location, through: Trip, as: 'planned_trips' }]
+//  });
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
@@ -40,14 +44,38 @@ router.get('/:id', async (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
+  Product.bulkCreate([
     {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+      product_name: 'Plain T-Shirt',
+      price: 14.99,
+      stock: 14,
+      category_id: 1,
+    },
+    {
+      product_name: 'Running Sneakers',
+      price: 90.0,
+      stock: 25,
+      category_id: 5,
+    },
+    {
+      product_name: 'Branded Baseball Hat',
+      price: 22.99,
+      stock: 12,
+      category_id: 4,
+    },
+    {
+      product_name: 'Top 40 Music Compilation Vinyl Record',
+      price: 12.99,
+      stock: 50,
+      category_id: 3,
+    },
+    {
+      product_name: 'Cargo Shorts',
+      price: 29.99,
+      stock: 22,
+      category_id: 2,
+    },
+  ])
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
